132a8c6bbc899990644217859dc120a7e3d6dd37
Abuhena Rony <Rony-CSE@users.noreply.github.com>
2018-07-13 15:51:30 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,12 @@
+<?php
+session_name('login');
+session_start();
 
+session_unset();
+session_destroy();
+
+// if user is not logged in then redirect to login page
+if (isset($_SESSION['login']) == false){
+    header("Location: login.php");
+}
+?>
